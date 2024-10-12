466a0f37bf5292bb548d9e4d7271641ff01d6244
Fractalo <42487844+fractalo@users.noreply.github.com>
2024-10-12 19:14:52 +0900

    Initial commit

---
+++ src/test/java/voyage/restock_notification/RestockNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package voyage.restock_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestockNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
